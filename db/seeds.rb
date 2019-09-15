a74c5c370c2784279e677ce7c1ad33222ae9e34b
Tyler Schaffer <tpschaffer@gmail.com>
2019-09-15 15:47:59 -0600

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+StudentCourse.destroy_all
+Course.destroy_all
+Student.destroy_all
+
+student_1 = Student.create!(name: 'Leslie Knope')
+student_2 = Student.create!(name: 'Michael Scott')
+student_3 = Student.create!(name: 'Jason Bourne')
+student_4 = Student.create!(name: 'Frodo')
+
+course_1 = Course.create!(name: 'Community Engagement')
+course_2 = Course.create!(name: 'Parks Management')
+
+student_course_1 = student_1.student_courses.create!(course: course_1, grade: 81.6)
+student_course_2 = student_2.student_courses.create!(course: course_1, grade: 56.9)
+student_course_3 = student_3.student_courses.create!(course: course_1, grade: 97.3)
+student_course_4 = student_4.student_courses.create!(course: course_1, grade: 89.9)
+
+student_course_5 = student_1.student_courses.create!(course: course_2, grade: 93.9)
+student_course_6 = student_3.student_courses.create!(course: course_2, grade: 81.1)
